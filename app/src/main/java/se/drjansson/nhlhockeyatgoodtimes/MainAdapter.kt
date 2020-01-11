b7bac7e8bfdb5ef34fe0df0e27dbfa58a02e255c
David Jansson <david.jansson@telia.com>
2020-01-11 17:04:13 +0100

    Changed from which field in the API result I'm showing the date from. It showed the wrong date earlier due to timezones.

--- app/src/main/java/se/drjansson/nhlhockeyatgoodtimes/MainAdapter.kt
+++ app/src/main/java/se/drjansson/nhlhockeyatgoodtimes/MainAdapter.kt
@@ -20,8 +20,11 @@ class MainAdapter( val matches: MainActivity.Matches) : RecyclerView.Adapter<Cus
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
         val awayTeam = matches.dates[position].games[0].teams.away.team.name
         val homeTeam = matches.dates[position].games[0].teams.home.team.name
-        val date = matches.dates[position].date
+        //val date = matches.dates[position].date
 //        val time = matches.dates[position].games[0].gameDate
+        val date = "" + matches.dates[position].games[0].cal.get(Calendar.YEAR) + "-"+
+                "" + (matches.dates[position].games[0].cal.get(Calendar.MONTH)+1) + "-" +
+                "" + matches.dates[position].games[0].cal.get(Calendar.DAY_OF_MONTH)
         val minute = matches.dates[position].games[0].cal.get(Calendar.MINUTE)
         val time = "" + matches.dates[position].games[0].cal.get(Calendar.HOUR_OF_DAY) + ":" +
                 if (minute < 10 ) "0$minute" else minute
